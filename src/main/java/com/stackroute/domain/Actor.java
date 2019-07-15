5de8c5b1e50d80d4d5dde3f27ec022b194d579d6
anshuman <289anshu441@gmail.com>
2019-07-15 09:53:30 +0530

    task1

---
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -0,0 +1,40 @@
+package com.stackroute.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Actor {
+
+    private String name;
+    private String gender;
+    private int age;
+
+    public Actor() {
+
+    }
+
+    public Actor(String name, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void printActor()
+    {
+        System.out.println("Actor's Name:"+name+"\n"+"Gender:"+gender+"\n"+"Age:"+age);
+    }
+
+}
---
+++ src/main/java/com/stackroute/domain/Config.java
@@ -0,0 +1,29 @@
+package com.stackroute.domain;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@ComponentScan(basePackages = "com.stackroute")
+public class Config {
+
+    @Bean
+    public Actor actor()
+    {
+        Actor actor =new Actor();
+        actor.setAge(22);
+        actor.setGender("male");
+        actor.setName("Agha");
+        return actor;
+    }
+    @Bean("movie1")
+    public Movie makeMovieObject()
+    {
+        Movie movie =new Movie(actor());
+        return movie;
+    }
+
+
+
+}
