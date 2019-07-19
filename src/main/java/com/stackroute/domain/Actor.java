d5fcdf482c84ccea38b38f37347d2e692b94fbfb
Anshuman3301 <51943125+Anshuman3301@users.noreply.github.com>
2019-07-19 09:53:57 +0530

    Update Actor.java

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Actor {
+    
 
+    //initailise one actor with its properties
     private String name;
     private String gender;
     private int age;
@@ -14,24 +16,30 @@ public class Actor {
 
     }
 
+    //create an actor object
     public Actor(String name, String gender, int age) {
         this.name = name;
         this.gender = gender;
         this.age = age;
     }
 
+    //set his name.
     public void setName(String name) {
         this.name = name;
     }
+    
 
+    //set his gender.
     public void setGender(String gender) {
         this.gender = gender;
     }
 
+    //set his age.
     public void setAge(int age) {
         this.age = age;
     }
 
+    //print the properties of the actor.
     public void printActor()
     {
         System.out.println("Actor's Name:"+name+"\n"+"Gender:"+gender+"\n"+"Age:"+age);
