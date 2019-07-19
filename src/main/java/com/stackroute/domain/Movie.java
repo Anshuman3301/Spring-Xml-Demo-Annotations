b57fd4d7efafa02dd7845605cf931a3ec234646a
Anshuman3301 <51943125+Anshuman3301@users.noreply.github.com>
2019-07-19 10:03:28 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -4,16 +4,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Movie {
+    
+    //initialise an actor object.
     private Actor actor;
 
+    //initialise the actor object inside movie class
     public Movie(Actor actor) {
         this.actor = actor;
     }
 
+    //set the actor
     public void setActor(Actor actor) {
         this.actor = actor;
     }
 
+    //print the actor
     public void printAboutMovieActor()
     {
         actor.printActor();
